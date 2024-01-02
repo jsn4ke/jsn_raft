7099994cff842f14589898aec951d7b90e1fb305
jsn4ke <mail.caoke@gmail.com>
2024-01-02 18:36:31 +0800

    添加pb代码生成工具

--- pb/raft.ext.go
+++ pb/raft.ext.go
@@ -12,82 +12,51 @@ var (
 	_ jsn_rpc.RpcUnit = (*AppendEntriesResponse)(nil)
 )
 
-// CmdId implements jsn_rpc.RpcUnit.
 func (*VoteRequest) CmdId() uint32 {
-	return uint32(Cmd_Cmd_VoterRequest)
+	return uint32(Cmd_Cmd_VoteRequest)
 }
-
-// Marshal implements jsn_rpc.RpcUnit.
 func (x *VoteRequest) Marshal() ([]byte, error) {
 	return proto.Marshal(x)
 }
-
-// New implements jsn_rpc.RpcUnit.
-func (*VoteRequest) New() jsn_rpc.RpcUnit {
-	return new(VoteRequest)
-}
-
-// Unmarshal implements jsn_rpc.RpcUnit.
 func (x *VoteRequest) Unmarshal(in []byte) error {
 	return proto.Unmarshal(in, x)
 }
-
-// CmdId implements jsn_rpc.RpcUnit.
+func (*VoteRequest) New() jsn_rpc.RpcUnit {
+	return new(VoteRequest)
+}
 func (*VoteResponse) CmdId() uint32 {
 	return uint32(Cmd_Cmd_VoteResponse)
 }
-
-// Marshal implements jsn_rpc.RpcUnit.
 func (x *VoteResponse) Marshal() ([]byte, error) {
 	return proto.Marshal(x)
 }
-
-// New implements jsn_rpc.RpcUnit.
-func (*VoteResponse) New() jsn_rpc.RpcUnit {
-	return new(VoteResponse)
-}
-
-// Unmarshal implements jsn_rpc.RpcUnit.
 func (x *VoteResponse) Unmarshal(in []byte) error {
 	return proto.Unmarshal(in, x)
 }
-
-// CmdId implements jsn_rpc.RpcUnit.
+func (*VoteResponse) New() jsn_rpc.RpcUnit {
+	return new(VoteResponse)
+}
 func (*AppendEntriesRequest) CmdId() uint32 {
 	return uint32(Cmd_Cmd_AppendEntriesRequest)
 }
-
-// Marshal implements jsn_rpc.RpcUnit.
 func (x *AppendEntriesRequest) Marshal() ([]byte, error) {
 	return proto.Marshal(x)
 }
-
-// New implements jsn_rpc.RpcUnit.
-func (*AppendEntriesRequest) New() jsn_rpc.RpcUnit {
-	return new(AppendEntriesRequest)
-}
-
-// Unmarshal implements jsn_rpc.RpcUnit.
 func (x *AppendEntriesRequest) Unmarshal(in []byte) error {
 	return proto.Unmarshal(in, x)
 }
-
-// CmdId implements jsn_rpc.RpcUnit.
+func (*AppendEntriesRequest) New() jsn_rpc.RpcUnit {
+	return new(AppendEntriesRequest)
+}
 func (*AppendEntriesResponse) CmdId() uint32 {
 	return uint32(Cmd_Cmd_AppendEntriesResponse)
 }
-
-// Marshal implements jsn_rpc.RpcUnit.
 func (x *AppendEntriesResponse) Marshal() ([]byte, error) {
 	return proto.Marshal(x)
 }
-
-// New implements jsn_rpc.RpcUnit.
-func (*AppendEntriesResponse) New() jsn_rpc.RpcUnit {
-	return new(AppendEntriesResponse)
-}
-
-// Unmarshal implements jsn_rpc.RpcUnit.
 func (x *AppendEntriesResponse) Unmarshal(in []byte) error {
 	return proto.Unmarshal(in, x)
+}
+func (*AppendEntriesResponse) New() jsn_rpc.RpcUnit {
+	return new(AppendEntriesResponse)
 }
